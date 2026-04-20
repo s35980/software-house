b27b4317cde036644e561c421ab0523dcbd7431c
s35980 <s35980@pjwstk.edu.pl>
2026-04-20 22:39:02 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        ZespolProjektowy zespol = new ZespolProjektowy("Campus App");
+
         Pracownik p1 = new Programista("E-101", "Anna", "Maj", 12000.0, "Java", 4);
         Pracownik p2 = new Tester("E-201", "Piotr", "Lis", 9800.0, true, 25);
         Pracownik p3 = new Programista("E-102", "Karol", "Wojcik", 11500.0, "Kotlin", 3);
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        zespol.dodajPracownika(p1);
+        zespol.dodajPracownika(p2);
+        zespol.dodajPracownika(p3);
 
-        System.out.println("Czy p1 to ta sama osoba co p2 (to samo ID)? " + p1.equals(p2));
+        zespol.wypiszSkladZespolu();
+        System.out.println("Łączny koszt zespołu: " + zespol.policzLacznyKoszt());
+
+        System.out.println(p1.przedstawSie());
+        System.out.println(p2.przedstawSie());
+
+        if (p1 instanceof Programista) {
+            Programista programista = (Programista) p1;
+            programista.wypiszTechnologie();
+        }
+
+        System.out.println(p2.equals(new Tester("E-201", "Inne", "Dane", 9000.0, false, 10)));
+        System.out.println(p2);
     }
 }
